4387513b28612c270841ff41406b2d3a09bdeb23
ljj <hhhh>
2018-08-22 13:31:08 +0800

    ddd

---
+++ app/src/main/java/com/example/administrator/text/Add.java
@@ -0,0 +1,8 @@
+package com.example.administrator.text;
+
+/**
+ * Created by ljj on 2018/8/17.
+ * Description:
+ */
+public class Add {
+}
---
+++ app/src/main/java/com/example/administrator/text/Main2Activity.java
@@ -0,0 +1,21 @@
+package com.example.administrator.text;
+
+import android.support.annotation.LayoutRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by ljj on 2018/8/17.
+ * Description:
+ */
+
+
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
