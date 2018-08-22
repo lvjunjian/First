86a818b398c293411bc6732b5dcdc902519d8cf5
ljj <hhhh>
2018-08-22 11:19:56 +0800

    bbb

--- app/src/main/java/com/example/administrator/text/Main4Activity.java
+++ app/src/main/java/com/example/administrator/text/Main4Activity.java
@@ -9,5 +9,8 @@ public class Main4Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+
+
+
     }
 }
